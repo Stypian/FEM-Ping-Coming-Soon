27c4e4b5f12354e0ea9d26ffc034c72758bf27e5
Stypian <157755550+Stypian@users.noreply.github.com>
2024-05-01 08:39:46 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,54 @@
+onload = function() {
+
+
+    const validateEmail = () => {
+        const inpt = document.querySelector(".ping-soon__email");
+        const form = document.querySelector(".ping-soon__email-wrap");
+        const submit = document.querySelector(".ping-soon__submit");
+        const error = document.querySelector(".ping-soon__error-p");
+        const valid = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,3})$/;
+
+        submit.addEventListener("click", function(e) {
+            e.preventDefault();
+            if (!valid.test(inpt.value)) {
+                inpt.style.borderColor = "hsl(354, 100%, 66%)";
+                error.style.visibility = "visible";
+            } else {
+                form.submit();
+            }
+        })
+    }
+
+    validateEmail();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
